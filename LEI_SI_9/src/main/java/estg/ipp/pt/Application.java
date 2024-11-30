32e5053b75591bf2e94d6988712762bce98ecf45
HUGO RICARDO ALMEIDA GUIMARÃES <8220337@estg.ipp.pt>
2024-11-30 18:20:11 +0000

    camunda springboot configuration

---
+++ LEI_SI_9/src/main/java/estg/ipp/pt/Application.java
@@ -0,0 +1,13 @@
+package estg.ipp.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
